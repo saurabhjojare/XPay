e7c331992112cdc8f96b82344cd667ba8fd1db0c
Saurabh Jojare <saurabh.jojare@gmail.com>
2025-08-30 11:31:56 +0530

    Gateway Fixes v1

--- xpay-backend/gateway/src/main/java/com/xpay/gateway/security/JwtTokenExtractor.java
+++ xpay-backend/gateway/src/main/java/com/xpay/gateway/security/JwtTokenExtractor.java
@@ -3,7 +3,7 @@ package com.xpay.gateway.security;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JwtTokenExtractor  {
+public class JwtTokenExtractor {
     public String extractToken(String authHeader) {
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             return authHeader.substring(7);
